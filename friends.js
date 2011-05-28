e946975594331d57781ce1be954c3bfcf18db28f
K4rtK <karthikkatooru@gmail.com>
2011-05-28 09:52:29 -0400

    dummy friends

---
+++ friends.js
@@ -0,0 +1,48 @@
+var friendsArr =  [
+ {
+         "name": "Mark Zuckerberg",
+         "id": "68310606562"
+      },
+      {
+         "name": "Emma Watson",
+         "id": "140216402663925"
+      },
+     {
+         "name": "James Gosling",
+         "id": "112693112079585"
+      },
+	 
+	  {
+         "name": "Steve Ballmer",
+         "id": "38345299923"
+      },
+	  {
+         "name": "Aishwarya Rai",
+         "id": "21867515179"
+      },
+	   {
+         "name": "Karthik Katooru",
+         "id": "100000444561277"
+      },
+	   {
+         "name": "Barrack Obama",
+         "id": "6815841748"
+      },
+      {
+         "name": "Richard Stallman",
+         "id": "19594649480"
+      },
+	   {
+         "name": "Sergey Brin",
+         "id": "104330069606260"
+      },
+	   {
+         "name": "Master Yoda",
+         "id": "189920071034833"
+      },
+	   {
+         "name": "Christian Bale",
+         "id": "112362215447738"
+      }
+	  
+   ];
